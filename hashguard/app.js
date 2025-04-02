140b8eff0c5d9e1c6a08f4c0bb25b5bfb31bd9e6
masterchiefff <anwarmagara@gmail.com>
2025-04-02 13:45:57 +0300

    allowed all origins

--- hashguard/app.js
+++ hashguard/app.js
@@ -97,7 +97,7 @@ const pendingPayments = new Map(); // checkoutRequestId -> { phone, amountKsh }
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://hash-guard.vercel.app/'],
+    origin: '*',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
 }));
